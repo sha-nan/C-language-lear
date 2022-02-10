11efd8dddf269dd430958e38799feb1ecceb5d34
shanan <1784935248@qq.com>
2022-02-10 10:52:59 +0800

    修改测试案例

--- DataStructure/link/test.c
+++ DataStructure/link/test.c
@@ -2,21 +2,14 @@
 
 int main(int argc, char **argv)
 {
-    int n;
     linklist head = list_create();
-    list_head_insert(head, 1);
-    list_head_insert(head, 2);
+    list_head_insert(head, 5);
     list_head_insert(head, 3);
+    list_head_insert(head, 1);
     list_show(head);
-
-    printf("pleas input delete pos:");
-    scanf("%d", &n);
-    if ((list_delete(head, n)) == -1)
-    {
-        printf("delete failed!\n");
-    }
-    list_show(head);
-    list_reverse(head);
+    list_order_insert(head, 4);
+    list_order_insert(head, 6);
+    list_order_insert(head, 0);
     list_show(head);
     return 0;
 }
