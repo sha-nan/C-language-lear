ee2235d44a6fca13c8bcee6e5d69d840a7e55ca2
shanan <1784935248@qq.com>
2022-02-11 09:15:09 +0800

    修改显示show函数

--- DataStructure/dlink/dlist.c
+++ DataStructure/dlink/dlist.c
@@ -1,22 +1,48 @@
 #include "dlist.h"
 
-
 /*
-函数名：dlist_create
-功能：创建一个空的双向链表
-参数：无
-返回值：返回一个空的双向链表
-*/
-dlistnode* dlist_create(void)
+ * 函数名称：dlist_create
+ * 函数功能：创建一个空的双向链表
+ * 输入参数：无
+ * 输出参数：无
+ * 返 回 值：返回一个空的双向链表
+ */
+dlistnode *dlist_create(void)
 {
-    dlistnode *head = (dlistnode*)malloc(sizeof(dlistnode));
-    if(head == NULL)
+    int num;
+    dlistnode *p, *r;
+    dlistnode *head = (dlistnode *)malloc(sizeof(dlistnode));
+    if (head == NULL)
     {
         printf("malloc error!\n");
-        exit(1);
+        return NULL;
     }
     head->next = head;
     head->prev = head;
+    r = head;
+
+    while (1)
+    {
+        printf("please input(-1 exit):");
+        scanf("%d", &num);
+        if (num == -1)
+        {
+            break;
+        }
+        if (p = (dlistnode *)malloc(sizeof(dlistnode)))
+        {
+            printf("malloc error!\n");
+            return NULL;
+        }
+        p->data = num;
+
+        p->prev = r;       // p->prev = head;
+        p->next = r->next; // p->next = head;
+        r->next = p;       //插入到链表尾部
+        head->prev = p;    //更新头节点的prev指针
+        r = p;             // r指向最后一个节点
+    }
+
     return head;
 }
 
@@ -28,6 +54,12 @@ dlistnode* dlist_create(void)
 */
 void dlist_show(dlistnode *head)
 {
-
+    dlistnode *p;
+    p = head->next;
+    while (p != head)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
 }
-
